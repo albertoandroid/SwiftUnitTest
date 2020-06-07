8d7ffa3ff4871cb65c52dcca8f2cd46853ace663
Alberto <info@androiddesdecero.com>
2020-06-07 14:06:53 +0200

    2

--- UnitTestSwiftCourse/UnitTestSwiftCourseTests/xctest/CalculatorTests.swift
+++ UnitTestSwiftCourse/UnitTestSwiftCourseTests/xctest/CalculatorTests.swift
@@ -6,6 +6,26 @@
 //  Copyright © 2020 Alberto Palomar Robledo. All rights reserved.
 //
 
+/*
+//METODO/////////////////ESPECIFICACION//////////////////////////////
+func sumar(                 |Este método devuelve un int
+    numero1: Int,           |
+    numero2: Int            |
+    ): Int                  | -> Nos devuelve un Entero con el resultado
+---------------------------------------------------------------------
+func restar(                |Este método devuelve un int r
+    numero1: Int,           |
+    numero2: Int            |
+    ): Int                  |-> Nos devuelve un Entero con el resultado
+---------------------------------------------------------------------
+ 
+Método a Probar                 |      Entrada      |       Salida Esperarada
+sumar(int a, int b)             |a = 10, b=20       |30
+sumar(int a, int b)             |a = 7, b=4         |11
+restar(int a, int b)            |a = 7, b=4         |3
+restar(int a, int b)            |a = 10, b=20       |-10
+*/
+
 import Foundation
 
 class CalculatorTests: XCTestCase {
